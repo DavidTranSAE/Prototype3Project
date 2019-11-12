8f052c71c8994768b30e28d4a09e3af0361456f5
David Tran <1016359@student.sae.edu.au>
2019-11-12 16:13:27 +1100

    First Person Character and Room Change Mechanic

---
+++ Prototype3/Assets/Scripts/AreaManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaManager : MonoBehaviour
+{
+    public GameObject bedroom1;
+    public GameObject bedroom2;
+    public GameObject tent;
+
+    private void OnEnable()
+    {
+        Player.moveRoom1 += moveRoom1;
+        
+    }
+
+    private void OnDisable()
+    {
+        Player.moveRoom1 -= moveRoom1;
+    }
+
+
+
+    void moveRoom1()
+    {
+        Destroy(bedroom1.gameObject);
+        bedroom2.transform.position = Vector3.zero;
+    }
+
+    void moveRoom2()
+    {
+
+    }
+}
---
+++ Prototype3/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public delegate void MoveRoom();
+    public static MoveRoom moveRoom1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Collision1")
+        {
+            moveRoom1();
+        }
+    }
+}
